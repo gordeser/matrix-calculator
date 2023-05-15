216fd12573e753341c13118b5c69febdfc9d1fc8
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-05-15 08:53:15 +0200

    Operation header file is added

---
+++ semestralka/src/Operations/Operation.h
@@ -0,0 +1,16 @@
+//
+// Created by gordeser on 13.05.2023.
+//
+
+#ifndef PA2_OPERATION_H
+#define PA2_OPERATION_H
+
+#include "../Matrices/Matrix.h"
+
+class Operation {
+public:
+    virtual ~Operation() {}
+    virtual std::shared_ptr<Matrix> execute(const std::shared_ptr<Matrix> &a) = 0;
+};
+
+#endif //PA2_OPERATION_H
