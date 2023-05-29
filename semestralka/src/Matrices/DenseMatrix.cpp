8c4e1d56f793c77557ee75d850825ef1aae3fd20
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-05-29 21:37:20 +0200

    dense matrix setval is implemented

--- semestralka/src/Matrices/DenseMatrix.cpp
+++ semestralka/src/Matrices/DenseMatrix.cpp
@@ -7,11 +7,19 @@
 DenseMatrix::DenseMatrix(std::size_t numRows, std::size_t numCols) : Matrix(numRows, numCols) {}
 
 double DenseMatrix::getVal(std::size_t row, std::size_t col) const {
+    // todo add check
     auto it = m_data.find(std::make_pair(row, col));
     if (it != m_data.end()) {
         return it->second;
     }
-    // call exception
+    // todo add smth if there is no value
     return 0;
 }
+
+void DenseMatrix::setVal(std::size_t row, std::size_t col, double val) {
+    // todo add check
+    m_data[std::make_pair(row, col)] = val;
+}
+
+
 
