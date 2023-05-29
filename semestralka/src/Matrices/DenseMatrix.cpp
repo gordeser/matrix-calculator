15f14c5d5ad25411e8e4a9020681fbe2267a5997
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-05-29 21:35:32 +0200

    dense matrix getval is implemented

--- semestralka/src/Matrices/DenseMatrix.cpp
+++ semestralka/src/Matrices/DenseMatrix.cpp
@@ -5,3 +5,13 @@
 #include "DenseMatrix.h"
 
 DenseMatrix::DenseMatrix(std::size_t numRows, std::size_t numCols) : Matrix(numRows, numCols) {}
+
+double DenseMatrix::getVal(std::size_t row, std::size_t col) const {
+    auto it = m_data.find(std::make_pair(row, col));
+    if (it != m_data.end()) {
+        return it->second;
+    }
+    // call exception
+    return 0;
+}
+
