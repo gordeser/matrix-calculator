7a67824854a974f5acc2dfaf6aa8045b35ab32d6
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-03 15:26:29 +0200

    DenseMatrix changed constructor

--- semestralka/src/Matrices/DenseMatrix.cpp
+++ semestralka/src/Matrices/DenseMatrix.cpp
@@ -4,7 +4,7 @@
 
 #include "DenseMatrix.h"
 
-DenseMatrix::DenseMatrix(std::size_t numRows, std::size_t numCols) : Matrix(numRows, numCols) {}
+DenseMatrix::DenseMatrix(std::size_t numRows, std::size_t numCols, std::vector<std::vector <double>> data) : Matrix(numRows, numCols), m_data(data) {}
 
 double DenseMatrix::getVal(std::size_t row, std::size_t col) const {
     return m_data[row][col];
