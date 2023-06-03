a29d214d840b0cbbc41e47a74748a25785b36fa9
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-03 14:18:46 +0200

    Execute for both matrices is done

--- semestralka/src/Operations/AdditionOperation.cpp
+++ semestralka/src/Operations/AdditionOperation.cpp
@@ -2,4 +2,20 @@
 // Created by gordeser on 15.05.2023.
 //
 
+#include <vector>
 #include "AdditionOperation.h"
+#include "../Exception.h"
+
+std::vector <std::vector <double>> AdditionOperation::execute(const std::shared_ptr<Matrix> &a, const std::shared_ptr<Matrix> &b) {
+    if (a->numRows() != b->numRows() || a->numCols() != b->numCols()) {
+        throw Exception("error");
+    }
+
+    std::vector <std::vector <double>> result (a->numRows(), std::vector <double> (a->numCols(), 0));
+    for (size_t i = 0; i < a->numRows(); ++i) {
+        for (size_t j = 0; j < a->numCols(); ++j) {
+            result[i][j] = a->getVal(i, j) + b->getVal(i, j);
+        }
+    }
+    return result;
+}
