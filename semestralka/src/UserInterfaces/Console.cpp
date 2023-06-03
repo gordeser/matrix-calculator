16783e87e6aeeb9b018de22a70baeccb5c56a08e
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-03 11:56:57 +0200

    showText is implemented

--- semestralka/src/UserInterfaces/Console.cpp
+++ semestralka/src/UserInterfaces/Console.cpp
@@ -2,15 +2,19 @@
 // Created by gordeser on 15.05.2023.
 //
 
+#include <iostream>
 #include "Console.h"
 
+void ConsoleUI::showText(const std::string &text) {
+    std::cout << text;
+}
 
 void ConsoleUI::showGreeting() {
-    std::cout << "Hello!" << std::endl;
-    std::cout << "Available commands you can get with input HELP" << std::endl;
-    std::cout << "Your input: ";
+    showText("Hello!\n");
+    showText("Available commands you can get with input HELP'\n");
+    showText("Your input: ");
 }
 
 void ConsoleUI::showFarewall() {
-    std::cout << "Goodbye!" << std::endl;
+    showText("Goodbye!\n");
 }
