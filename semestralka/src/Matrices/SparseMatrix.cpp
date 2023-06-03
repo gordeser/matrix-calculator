64343f15923140b63edb0d964794e77dd23f4b8b
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-03 15:45:03 +0200

    SparseMatrix getVal handle exception is done

--- semestralka/src/Matrices/SparseMatrix.cpp
+++ semestralka/src/Matrices/SparseMatrix.cpp
@@ -2,13 +2,17 @@
 // Created by gordeser on 14.05.2023.
 //
 
+#include <stdexcept>
 #include "SparseMatrix.h"
 
 SparseMatrix::SparseMatrix(std::size_t numRows, std::size_t numCols) : Matrix(numRows, numCols) {}
 
 double SparseMatrix::getVal(std::size_t row, std::size_t col) const {
-    // todo handle exception
-    return m_data.at(std::make_pair(row, col));
+    try {
+        return m_data.at(std::make_pair(row, col));
+    } catch(std::out_of_range &e) {
+        return 0;
+    }
 }
 
 std::shared_ptr<Matrix> SparseMatrix::clone() const {
