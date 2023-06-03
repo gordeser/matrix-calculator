ea2652c905690efb4acda896ae9284317667eddd
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-03 15:40:20 +0200

    IdentityMatrix getVal is implemented

--- semestralka/src/Matrices/IdentityMatrix.cpp
+++ semestralka/src/Matrices/IdentityMatrix.cpp
@@ -2,6 +2,14 @@
 // Created by gordeser on 14.05.2023.
 //
 
+#include <vector>
 #include "IdentityMatrix.h"
 
 IdentityMatrix::IdentityMatrix(std::size_t numRows, std::size_t numCols) : Matrix(numRows, numCols) {}
+
+double IdentityMatrix::getVal(std::size_t row, std::size_t col) const {
+    if (row == col)
+        return 1;
+    return 0;
+}
+
