91144361e20a976c2114355952f1e24fa7f9d135
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-03 15:38:59 +0200

    SparseMatrix getVal is implemented

--- semestralka/src/Matrices/SparseMatrix.cpp
+++ semestralka/src/Matrices/SparseMatrix.cpp
@@ -5,3 +5,9 @@
 #include "SparseMatrix.h"
 
 SparseMatrix::SparseMatrix(std::size_t numRows, std::size_t numCols) : Matrix(numRows, numCols) {}
+
+double SparseMatrix::getVal(std::size_t row, std::size_t col) const {
+    // todo handle exception
+    return m_data.at(std::make_pair(row, col));
+}
+
