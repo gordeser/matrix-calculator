846b503c5a35f53ca35cfd84323885f4af7c3948
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 11:48:31 +0200

    IdentityMatrix getVal method is modified

--- semestralka/src/Matrices/IdentityMatrix.cpp
+++ semestralka/src/Matrices/IdentityMatrix.cpp
@@ -2,15 +2,12 @@
 // Created by gordeser on 14.05.2023.
 //
 
-#include <vector>
 #include "IdentityMatrix.h"
 
 IdentityMatrix::IdentityMatrix(std::size_t numRows, std::size_t numCols) : Matrix(numRows, numCols) {}
 
 double IdentityMatrix::getVal(std::size_t row, std::size_t col) const {
-    if (row == col)
-        return 1;
-    return 0;
+    return (row == col) ? 1 : 0;
 }
 
 std::size_t IdentityMatrix::rank() const {
