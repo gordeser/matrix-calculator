f88d9d973b4d59c9c4a431ad76e322183ea6015c
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 16:03:43 +0200

    Parser executeOperations is implemented

--- semestralka/src/Parser.cpp
+++ semestralka/src/Parser.cpp
@@ -130,16 +130,242 @@ void Parser::parseInput(std::string input) {
         if (matrix != nullptr) {
             m_storage.addMatrix(name, matrix);
             return;
-        }
+        } else
+            throw ParserException("Unknown command. Try \"help\"\n");
     } else {
         auto matrix = executeOperations(tokens);
 
         if (matrix != nullptr) {
             m_console.showText(matrix->print(""));
             return;
-        }
+        } else
+            throw ParserException("Unknown command. Try \"help\"\n");
     }
-    throw ParserException("Unknown command. Try \"help\"\n");
+}
+
+
+std::shared_ptr<Matrix> Parser::executeOperations(std::vector<std::string> &elements) {
+    std::string operationName = elements[0];
+    std::shared_ptr <Operation> operation;
+
+    if (operationName == Commands::TRANS) {
+        if (elements.size() < 2)
+            throw ParserException("USAGE: trans <matrix>\n");
+
+        auto operands = std::vector<std::string>(elements.begin()+1, elements.end());
+        std::shared_ptr<Matrix> operand;
+
+        if (operands.size() > 1) {
+            operand = executeOperations(operands);
+            if (operand == nullptr)
+                throw ParserException("There is an error in nested operations\n");
+        } else
+            operand = m_storage.getMatrix(operands[0]);
+
+        operation = std::make_shared<TransposeOperation>(operand);
+        auto result = m_utilities.createMatrix(operation->execute());
+        return result;
+    } else if (operationName == Commands::INV) {
+        if (elements.size() < 2)
+            throw ParserException("USAGE: inv <matrix>\n");
+
+        auto operands = std::vector<std::string>(elements.begin()+1, elements.end());
+        std::shared_ptr<Matrix> operand;
+
+        if (operands.size() > 1) {
+            operand = executeOperations(operands);
+            if (operand == nullptr)
+                throw ParserException("There is an error in nested operations\n");
+        } else
+            operand = m_storage.getMatrix(operands[0]);
+
+        operation = std::make_shared<InversionOperation>(operand);
+        auto result = m_utilities.createMatrix(operation->execute());
+        return result;
+    } else if (operationName == Commands::GEM) {
+        if (elements.size() < 2)
+            throw ParserException("USAGE: gem <matrix>\n");
+
+        auto operands = std::vector<std::string>(elements.begin()+1, elements.end());
+        std::shared_ptr<Matrix> operand;
+
+        if (operands.size() > 1) {
+            operand = executeOperations(operands);
+            if (operand == nullptr)
+                throw ParserException("There is an error in nested operations\n");
+        } else
+            operand = m_storage.getMatrix(operands[0]);
+
+        operation = std::make_shared<GaussEliminationOperation>(operand);
+        auto result = m_utilities.createMatrix(operation->execute());
+        return result;
+
+
+    } else if (operationName == Commands::ADD) {
+        if (elements.size() < 3)
+            throw ParserException("USAGE: add <matrix> <matrix>\n");
+
+        auto firstOperand = m_storage.getMatrix(elements[1]);
+        auto nextOperands = std::vector<std::string>(elements.begin()+2, elements.end());
+        std::shared_ptr<Matrix> secondOperand;
+
+        if (nextOperands.size() > 1) {
+            secondOperand = executeOperations(nextOperands);
+            if (secondOperand == nullptr)
+                throw ParserException("There is an error in nested operations\n");
+        } else
+            secondOperand = m_storage.getMatrix(nextOperands[0]);
+
+        operation = std::make_shared<AdditionOperation>(firstOperand, secondOperand);
+        auto result = m_utilities.createMatrix(operation->execute());
+        return result;
+    } else if (operationName == Commands::SUB) {
+        if (elements.size() < 3)
+            throw ParserException("USAGE: sub <matrix> <matrix>\n");
+
+        auto firstOperand = m_storage.getMatrix(elements[1]);
+        auto nextOperands = std::vector<std::string>(elements.begin()+2, elements.end());
+        std::shared_ptr<Matrix> secondOperand;
+
+        if (nextOperands.size() > 1) {
+            secondOperand = executeOperations(nextOperands);
+            if (secondOperand == nullptr)
+                throw ParserException("There is an error in nested operations\n");
+        } else
+            secondOperand = m_storage.getMatrix(nextOperands[0]);
+
+        operation = std::make_shared<SubtractionOperation>(firstOperand, secondOperand);
+        auto result = m_utilities.createMatrix(operation->execute());
+        return result;
+    } else if (operationName == Commands::MUL) {
+        if (elements.size() < 3)
+            throw ParserException("USAGE: mul <number> <matrix> OR mul <matrix> <matrix>\n");
+
+        try {
+            double numOperand = std::stod(elements[1]);
+            auto nextOperands = std::vector<std::string>(elements.begin()+2, elements.end());
+            std::shared_ptr <Matrix> matrixOperand;
+
+            if (nextOperands.size() > 1) {
+                matrixOperand = executeOperations(nextOperands);
+                if (matrixOperand == nullptr)
+                    throw ParserException("There is an error in nested operations\n");
+            } else
+                matrixOperand = m_storage.getMatrix(nextOperands[0]);
+
+            operation = std::make_shared<MultiplicationNumberOperation>(matrixOperand, numOperand);
+        } catch (std::invalid_argument &e) {
+            auto firstOperand = m_storage.getMatrix(elements[1]);
+            auto nextOperands = std::vector<std::string>(elements.begin()+2, elements.end());
+            std::shared_ptr <Matrix> secondOperand;
+
+            if (nextOperands.size() > 1) {
+                secondOperand = executeOperations(nextOperands);
+                if (secondOperand == nullptr)
+                    throw ParserException("There is an error in nested operations\n");
+            } else
+                secondOperand = m_storage.getMatrix(nextOperands[0]);
+
+            operation = std::make_shared<MultiplicationOperation>(firstOperand, secondOperand);
+        } catch (std::out_of_range &e) {
+            throw ParserException("Numbers are too large\n");
+        }
+
+        auto result = m_utilities.createMatrix(operation->execute());
+        return result;
+    } else if (operationName == Commands::JOINRIGHT) {
+        if (elements.size() < 3)
+            throw ParserException("USAGE: joinright <matrix> <matrix>\n");
+
+        auto firstOperand = m_storage.getMatrix(elements[1]);
+        auto nextOperands = std::vector<std::string>(elements.begin()+2, elements.end());
+        std::shared_ptr<Matrix> secondOperand;
+
+        if (nextOperands.size() > 1) {
+            secondOperand = executeOperations(nextOperands);
+            if (secondOperand == nullptr)
+                throw ParserException("There is an error in nested operations\n");
+        } else
+            secondOperand = m_storage.getMatrix(nextOperands[0]);
+
+        operation = std::make_shared<JoiningRightOperation>(firstOperand, secondOperand);
+        auto result = m_utilities.createMatrix(operation->execute());
+        return result;
+    } else if (operationName == Commands::JOINDOWN) {
+        if (elements.size() < 3)
+            throw ParserException("USAGE: joindown <matrix> <matrix>\n");
+
+        auto firstOperand = m_storage.getMatrix(elements[1]);
+        auto nextOperands = std::vector<std::string>(elements.begin()+2, elements.end());
+        std::shared_ptr<Matrix> secondOperand;
+
+        if (nextOperands.size() > 1) {
+            secondOperand = executeOperations(nextOperands);
+            if (secondOperand == nullptr)
+                throw ParserException("There is an error in nested operations\n");
+        } else
+            secondOperand = m_storage.getMatrix(nextOperands[0]);
+
+        operation = std::make_shared<JoiningDownOperation>(firstOperand, secondOperand);
+        auto result = m_utilities.createMatrix(operation->execute());
+        return result;
+    } else if (operationName == Commands::TRIM) {
+        if (elements.size() < 6)
+            throw ParserException("USAGE: trim <matrix> <num_rows> <num_cols> <offset_rows> <offset_cols>\n");
+
+        size_t numRows, numCols, offsetRows, offsetCols;
+        try {
+            numRows = std::stoull(elements[elements.size()-4]);
+            numCols = std::stoull(elements[elements.size()-3]);
+            offsetRows = std::stoull(elements[elements.size()-2]);
+            offsetCols = std::stoull(elements[elements.size()-1]);
+        } catch (std::invalid_argument &e) {
+            throw ParserException("USAGE: trim <matrix> <num_rows> <num_cols> <offset_rows> <offset_cols>\n");
+        } catch (std::out_of_range &e) {
+            throw ParserException("Numbers are too large\n");
+        }
+
+        auto nextOperands = std::vector<std::string>(elements.begin()+1, elements.end()-4);
+        std::shared_ptr <Matrix> matrixOperand;
+
+        if (nextOperands.size() > 1) {
+            matrixOperand = executeOperations(nextOperands);
+            if (matrixOperand == nullptr)
+                throw ParserException("There is an error in nested operations\n");
+        } else
+            matrixOperand = m_storage.getMatrix(nextOperands[0]);
+
+        operation = std::make_shared<TrimmingOperation>(matrixOperand, numRows, numCols, offsetRows, offsetCols);
+        auto result = m_utilities.createMatrix(operation->execute());
+        return result;
+    } else if (operationName == Commands::EXP) {
+        if (elements.size() < 3)
+            throw ParserException("USAGE: exp <matrix> <number>\n");
+
+        double numOperand;
+        try {
+            numOperand = std::stod(elements[elements.size()-1]);
+        } catch (std::invalid_argument &e) {
+            throw ParserException("USAGE: exp <matrix> <number>\n");
+        } catch (std::out_of_range &e) {
+            throw ParserException("Numbers are too large\n");
+        }
+
+        auto nextOperands = std::vector<std::string>(elements.begin()+1, elements.end()-1);
+        std::shared_ptr<Matrix> matrixOperand;
+
+        if (nextOperands.size() > 1) {
+            matrixOperand = executeOperations(nextOperands);
+            if (matrixOperand == nullptr)
+                throw ParserException("There is an error in nested operations\n");
+        } else
+            matrixOperand = m_storage.getMatrix(nextOperands[0]);
+
+        operation = std::make_shared<ExponentiationOperation>(matrixOperand, numOperand);
+        auto result = m_utilities.createMatrix(operation->execute());
+        return result;
+    }
+    return nullptr;
 }
 
 
