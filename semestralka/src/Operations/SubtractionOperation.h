37005144931c33a6bddf2c20714b91dd33c1881a
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 13:04:01 +0200

    SubtractionOperation header file is modified

--- semestralka/src/Operations/SubtractionOperation.h
+++ semestralka/src/Operations/SubtractionOperation.h
@@ -5,13 +5,12 @@
 #ifndef PA2_SUBTRACTIONOPERATION_H
 #define PA2_SUBTRACTIONOPERATION_H
 
-#include <vector>
 #include "BinaryOperation.h"
 
 class SubtractionOperation  : public BinaryOperation {
 public:
-    std::vector <std::vector <double>> execute(const std::shared_ptr<Matrix> &a, const std::shared_ptr<Matrix> &b) override;
-    std::vector <std::vector <double>> execute(const std::shared_ptr<Matrix> &a, double b) override;
+    SubtractionOperation(const std::shared_ptr<Matrix> &lhs, const std::shared_ptr<Matrix> &rhs);
+    std::vector <std::vector <double>> execute() override;
 };
 
 #endif //PA2_SUBTRACTIONOPERATION_H
