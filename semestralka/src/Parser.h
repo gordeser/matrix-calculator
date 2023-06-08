0240db0554f1b8936c842c1a04e2e66e6c0206e8
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 12:50:42 +0200

    Parser header file is modified

--- semestralka/src/Parser.h
+++ semestralka/src/Parser.h
@@ -7,10 +7,31 @@
 
 #include <vector>
 #include <string>
+#include "MatrixStorage.h"
+#include "UserInterfaces/ConsoleUI.h"
+#include "Utilities.h"
+#include "Exports/TextExport.h"
 
 class Parser {
+    MatrixStorage m_storage;
+    ConsoleUI m_console;
+    Utilities m_utilities;
+    TextExport m_textexport;
+
+    std::shared_ptr<Matrix> executeOperations(std::vector <std::string> &operations);
+    void printHelp();
+    void printElements(const std::vector <std::string> &elements);
+    void printAllElements();
+    void exportElements(const std::string &filename, const std::vector <std::string> &elements);
+    void exportAllElements(const std::string &filename);
+    void deleteElements(const std::vector <std::string> &elements);
+    void deleteAllElements();
+    void importElements(const std::vector<std::string> &elements);
+    void scanElements(const std::string &name);
+    void printDet(const std::vector <std::string> &elements);
+    void printRank(const std::vector <std::string> &elements);
 public:
-    std::vector<std::string> parseInput();
+    void parseInput(std::string input);
 };
 
 #endif //PA2_PARSER_H
