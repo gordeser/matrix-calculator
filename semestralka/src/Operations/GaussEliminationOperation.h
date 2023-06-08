df3c8b122ac234b9ec2f6e86f2062d5fb663c4aa
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 13:07:36 +0200

    GaussEliminationOperation header file is modified

--- semestralka/src/Operations/GaussEliminationOperation.h
+++ semestralka/src/Operations/GaussEliminationOperation.h
@@ -5,11 +5,16 @@
 #ifndef PA2_GAUSSELIMINATIONOPERATION_H
 #define PA2_GAUSSELIMINATIONOPERATION_H
 
-#include <vector>
 #include "Operation.h"
 
 class GaussEliminationOperation : public Operation {
-    std::vector <std::vector <double>> execute(const std::shared_ptr<Matrix> &a) override;
+    void swapRows(std::vector<std::vector <double>> &matrix, size_t firstRow, size_t secondRow);
+    void scaleRow(std::vector<std::vector <double>> &matrix, size_t row, double number);
+    void addScaledRow(std::vector<std::vector <double>> &matrix, size_t sourceRow, size_t targetRow, double number);
+    void reduceRowEchelon(std::vector <std::vector <double>> &matrix);
+public:
+    GaussEliminationOperation(const std::shared_ptr <Matrix> &lhs);
+    std::vector <std::vector <double>> execute() override;
 };
 
 #endif //PA2_GAUSSELIMINATIONOPERATION_H
