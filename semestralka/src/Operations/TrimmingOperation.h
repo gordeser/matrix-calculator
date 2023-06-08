7ce6a6ff997061be8f395c8052c5a8202abe0f36
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 13:02:50 +0200

    TrimmingOperation header file is modified

--- semestralka/src/Operations/TrimmingOperation.h
+++ semestralka/src/Operations/TrimmingOperation.h
@@ -5,11 +5,14 @@
 #ifndef PA2_TRIMMINGOPERATION_H
 #define PA2_TRIMMINGOPERATION_H
 
-#include <vector>
-#include "BinaryOperation.h"
+#include "Operation.h"
 
-class TrimmingOperation : public BinaryOperation {
-    std::vector <std::vector <double>> execute(const std::shared_ptr<Matrix> &a, std::pair<std::size_t, std::size_t> &b);
+class TrimmingOperation : public Operation {
+protected:
+    size_t m_rows, m_cols, m_offsetRows, m_offsetCols;
+public:
+    TrimmingOperation(std::shared_ptr<Matrix> &lhs, size_t rows, size_t cols, size_t offsetRows, size_t offsetCols);
+    std::vector <std::vector <double>> execute();
 };
 
 #endif //PA2_TRIMMINGOPERATION_H
