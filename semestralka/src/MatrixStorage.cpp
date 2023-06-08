1b7bea0f727606535134d8f18d4d4b5720d4c931
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 12:16:00 +0200

    MatrixStorage removeMatrix is implemented

--- semestralka/src/MatrixStorage.cpp
+++ semestralka/src/MatrixStorage.cpp
@@ -8,4 +8,10 @@
 void MatrixStorage::addMatrix(const std::string &name, const std::shared_ptr<Matrix> &matrix) {
     m_data[name] = matrix;
 }
+
+void MatrixStorage::removeMatrix(const std::string &name) {
+    if (m_data.find(name) == m_data.end())
+        throw StorageException("There is no matrix with name " + name + "\n");
+    m_data.erase(name);
+}
 
