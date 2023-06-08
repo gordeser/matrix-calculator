0a91229985790f94911b42dc3bd89f645712aadf
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 13:01:46 +0200

    ExponentiationOperation is implemented

--- semestralka/src/Operations/ExponentiationOperation.cpp
+++ semestralka/src/Operations/ExponentiationOperation.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "ExponentiationOperation.h"
+#include "MultiplicationOperation.h"
+#include "../Exceptions/OperationException.h"
+
+ExponentiationOperation::ExponentiationOperation(const std::shared_ptr<Matrix> &lhs, int rhs): Operation(lhs), m_rhs(rhs) {}
+
+std::vector <std::vector <double>> ExponentiationOperation::execute() {
+    if (m_lhs->numRows() != m_lhs->numCols())
+        throw OperationException("Matrix must be square\n");
+
+    std::vector <std::vector <double>> result(m_lhs->numRows(), std::vector <double> (m_lhs->numCols(), 0));
+    for (size_t i = 0; i < m_lhs->numRows(); ++i)
+        result[i][i] = 1;
+
+    for (int i = 0; i < m_rhs; ++i)
+        result = MultiplicationOperation(m_utilities.createMatrix(result), m_lhs->clone()).execute();
+    return result;
+}
