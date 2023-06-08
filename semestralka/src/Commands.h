adb8efe86f0edf16811b1d20ec4409d0f5dbbd08
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 12:06:21 +0200

    Commands modified to lowercase

--- semestralka/src/Commands.h
+++ semestralka/src/Commands.h
@@ -9,28 +9,29 @@
 
 class Commands {
 public:
-    inline static const std::string ADD = "ADD";
-    inline static const std::string SUB = "SUB";
-    inline static const std::string MUL = "MUL";
-    inline static const std::string JOINRIGHT = "JOINRIGHT";
-    inline static const std::string JOINDOWN = "JOINDOWN";
-    inline static const std::string TRIM = "TRIM";
-    inline static const std::string EXP = "EXP";
-    inline static const std::string TRANS = "TRANS";
-    inline static const std::string INV = "INV";
-    inline static const std::string DET = "DET";
-    inline static const std::string RANK = "RANK";
-    inline static const std::string GEM = "GEM";
-    inline static const std::string SCAN = "SCAN";
-    inline static const std::string IMPORT = "IMPORT";
-    inline static const std::string DEL = "DEL";
-    inline static const std::string DELALL = "DELALL";
-    inline static const std::string EXPORT = "EXPORT";
-    inline static const std::string EXPORTALL = "EXPORTALL";
-    inline static const std::string PRINT = "PRINT";
-    inline static const std::string PRINTALL = "PRINTALL";
-    inline static const std::string HELP = "HELP";
-    inline static const std::string EXIT = "EXIT";
+    inline static const std::string ADD = "add";
+    inline static const std::string SUB = "sub";
+    inline static const std::string MUL = "mul";
+    inline static const std::string JOINRIGHT = "joinright";
+    inline static const std::string JOINDOWN = "joindown";
+    inline static const std::string TRIM = "trim";
+    inline static const std::string EXP = "exp";
+    inline static const std::string TRANS = "trans";
+    inline static const std::string INV = "inv";
+    inline static const std::string DET = "det";
+    inline static const std::string RANK = "rank";
+    inline static const std::string GEM = "gem";
+    inline static const std::string SCAN = "scan";
+    inline static const std::string IMPORT = "import";
+    inline static const std::string DEL = "del";
+    inline static const std::string DELALL = "delall";
+    inline static const std::string EXPORT = "export";
+    inline static const std::string EXPORTALL = "exportall";
+    inline static const std::string PRINT = "print";
+    inline static const std::string PRINTALL = "printall";
+    inline static const std::string HELP = "help";
+    inline static const std::string EXIT = "exit";
+    inline static const std::string QUIT = "quit";
 };
 
 #endif //PA2_COMMANDS_H
