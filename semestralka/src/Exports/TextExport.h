1a9d14b31331ee2556f99142eb9b8a970530dd9f
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 12:25:10 +0200

    TextExport header file is implemented

--- semestralka/src/Exports/TextExport.h
+++ semestralka/src/Exports/TextExport.h
@@ -6,10 +6,13 @@
 #define PA2_TEXTEXPORT_H
 
 #include "Export.h"
+#include "../Utilities.h"
 
 class TextExport : public Export {
+    Utilities m_utilities;
 public:
-    void exportData(const std::shared_ptr<Matrix> &matrix, const std::string &filename) override;
+    void exportData(const std::string &filename, const std::vector <std::string> &names, const MatrixStorage &storage) override;
+    void importData(const std::vector<std::string> &filenames, MatrixStorage &storage) override;
 };
 
 #endif //PA2_TEXTEXPORT_H
