c97406227392e51bb55901c33c2370b5ef686c71
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 14:34:23 +0200

    TransposeOperation is implemented

--- semestralka/src/Operations/TransposeOperation.cpp
+++ semestralka/src/Operations/TransposeOperation.cpp
@@ -4,12 +4,13 @@
 
 #include "TransposeOperation.h"
 
-std::vector <std::vector <double>> TransposeOperation::execute(const std::shared_ptr<Matrix> &a) {
-    std::vector <std::vector <double>> result(a->numRows(), std::vector<double> (a->numCols(), 0));
-    for (size_t i = 0; i < a->numRows(); ++i) {
-        for (size_t j = 0; j < a->numCols(); ++j) {
-            result[i][j] = a->getVal(j, i);
-        }
-    }
+TransposeOperation::TransposeOperation(std::shared_ptr<Matrix> lhs) : Operation(lhs) {}
+
+std::vector <std::vector <double>> TransposeOperation::execute() {
+    std::vector <std::vector <double>> result(m_lhs->numRows(), std::vector<double> (m_lhs->numCols(), 0));
+    for (size_t i = 0; i < m_lhs->numRows(); ++i)
+        for (size_t j = 0; j < m_lhs->numCols(); ++j)
+            result[i][j] = m_lhs->getVal(j, i);
+
     return result;
 }
