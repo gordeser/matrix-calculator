6ea013c88e891f3d283fea96e4211e62e48643cb
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 12:23:11 +0200

    ConsoleUI methods are implemented

---
+++ semestralka/src/UserInterfaces/ConsoleUI.cpp
@@ -0,0 +1,20 @@
+//
+// Created by gordeser on 15.05.2023.
+//
+
+#include <iostream>
+#include <algorithm>
+#include "ConsoleUI.h"
+
+void ConsoleUI::showText(const std::string &text) const {
+    std::cout << text;
+}
+
+std::string ConsoleUI::getInput() {
+    std::string input;
+    std::getline(std::cin, input);
+    std::transform(input.begin(), input.end(), input.begin(), ::tolower);
+    if (std::cin.eof())
+        return "quit";
+    return input;
+}
