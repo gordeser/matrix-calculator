720dbe9c96fa6c289e784f80a98d73b0cb6d1f1f
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 14:37:20 +0200

    MultiplicationOperation is implemented

--- semestralka/src/Operations/MultiplicationOperation.cpp
+++ semestralka/src/Operations/MultiplicationOperation.cpp
@@ -3,31 +3,23 @@
 //
 
 #include "MultiplicationOperation.h"
-#include "../Exception.h"
+#include "../Exceptions/OperationException.h"
 
-std::vector <std::vector <double>> MultiplicationOperation::execute(const std::shared_ptr<Matrix> &a, const std::shared_ptr<Matrix> &b) {
-    if (a->numCols() != b->numRows())
-        throw Exception("error");
+MultiplicationOperation::MultiplicationOperation(const std::shared_ptr<Matrix> &lhs, const std::shared_ptr<Matrix> &rhs) : BinaryOperation(lhs, rhs) {}
 
-    std::vector <std::vector <double>> result(a->numRows(), std::vector <double> (b->numCols(), 0));
+std::vector <std::vector <double>> MultiplicationOperation::execute() {
+    if (m_lhs->numCols() != m_rhs->numRows())
+        throw OperationException("Number of first matrix columns must be equal to number of second matrix rows\n");
 
-    for (size_t i = 0; i < a->numRows(); ++i) {
-        for (size_t j = 0; j < b->numCols(); ++j) {
+    std::vector <std::vector <double>> result(m_lhs->numRows(), std::vector <double> (m_rhs->numCols(), 0));
+
+    for (size_t i = 0; i < m_lhs->numRows(); ++i) {
+        for (size_t j = 0; j < m_rhs->numCols(); ++j) {
             double sum = 0;
-            for (size_t k = 0; k < a->numCols(); ++k) {
-                sum += a->getVal(i, k) * b->getVal(k, j);
-            }
+            for (size_t k = 0; k < m_lhs->numCols(); ++k)
+                sum += m_lhs->getVal(i, k) * m_rhs->getVal(k, j);
+            
             result[i][j] = sum;
-        }
-    }
-    return result;
-}
-
-std::vector <std::vector <double>> MultiplicationOperation::execute(const std::shared_ptr<Matrix> &a, double b) {
-    std::vector <std::vector <double>> result(a->numRows(), std::vector<double> (a->numCols(), 0));
-    for (size_t i = 0; i < a->numRows(); ++i) {
-        for (size_t j = 0; j < a->numCols(); ++j) {
-            result[i][j] = a->getVal(i, j) * b;
         }
     }
     return result;
