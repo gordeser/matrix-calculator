832891d92bc98f5e00559aa0146323e3bd7461a0
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 12:56:05 +0200

    AdditionOperation can be executed only on both matrices

--- semestralka/src/Operations/AdditionOperation.cpp
+++ semestralka/src/Operations/AdditionOperation.cpp
@@ -2,30 +2,21 @@
 // Created by gordeser on 15.05.2023.
 //
 
-#include <vector>
 #include "AdditionOperation.h"
-#include "../Exception.h"
+#include "../Exceptions/OperationException.h"
 
-std::vector <std::vector <double>> AdditionOperation::execute(const std::shared_ptr<Matrix> &a, const std::shared_ptr<Matrix> &b) {
-    if (a->numRows() != b->numRows() || a->numCols() != b->numCols()) {
-        throw Exception("error");
-    }
+AdditionOperation::AdditionOperation(const std::shared_ptr<Matrix> &lhs, const std::shared_ptr<Matrix> &rhs) : BinaryOperation(lhs, rhs) {}
 
-    std::vector <std::vector <double>> result (a->numRows(), std::vector <double> (a->numCols(), 0));
-    for (size_t i = 0; i < a->numRows(); ++i) {
-        for (size_t j = 0; j < a->numCols(); ++j) {
-            result[i][j] = a->getVal(i, j) + b->getVal(i, j);
-        }
-    }
-    return result;
-}
+std::vector <std::vector <double>> AdditionOperation::execute() {
+    if (m_lhs->numRows() != m_rhs->numRows())
+        throw OperationException("Number of rows must be equal\n");
 
-std::vector <std::vector <double>> AdditionOperation::execute(const std::shared_ptr<Matrix> &a, double b) {
-    std::vector <std::vector <double>> result (a->numRows(), std::vector <double> (a->numCols(), 0));
-    for (size_t i = 0; i < a->numRows(); ++i) {
-        for (size_t j = 0; j < a->numCols(); ++j) {
-            result[i][j] = a->getVal(i, j) + b;
-        }
-    }
+    if (m_lhs->numCols() != m_rhs->numCols())
+        throw OperationException("Number of columns must be equal\n");
+
+    std::vector <std::vector <double>> result (m_lhs->numRows(), std::vector <double> (m_lhs->numCols(), 0));
+    for (size_t i = 0; i < m_lhs->numRows(); ++i)
+        for (size_t j = 0; j < m_lhs->numCols(); ++j)
+            result[i][j] = m_lhs->getVal(i, j) + m_rhs->getVal(i, j);
     return result;
 }
