174ddd868bd9a7a4632ab6c781f16d191b1d250d
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 12:15:13 +0200

    MatrixStorage addMatrix method is implemented

--- semestralka/src/MatrixStorage.cpp
+++ semestralka/src/MatrixStorage.cpp
@@ -3,3 +3,9 @@
 //
 
 #include "MatrixStorage.h"
+#include "Exceptions/StorageException.h"
+
+void MatrixStorage::addMatrix(const std::string &name, const std::shared_ptr<Matrix> &matrix) {
+    m_data[name] = matrix;
+}
+
