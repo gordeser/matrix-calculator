c052e381cd50ab84795cd4a84384c181a620dc72
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 12:53:46 +0200

    BinaryOperator constructor is implemented

---
+++ semestralka/src/Operations/BinaryOperation.cpp
@@ -0,0 +1,7 @@
+//
+// Created by duama on 05.06.2023.
+//
+
+#include "BinaryOperation.h"
+
+BinaryOperation::BinaryOperation(std::shared_ptr<Matrix> lhs, std::shared_ptr<Matrix> rhs) : Operation(lhs), m_rhs(rhs) {}
