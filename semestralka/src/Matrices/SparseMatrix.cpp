686b29a59d31089dc4148ab3a3191178b696b4d8
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 12:02:12 +0200

    SparseMatrix print method is implemented

--- semestralka/src/Matrices/SparseMatrix.cpp
+++ semestralka/src/Matrices/SparseMatrix.cpp
@@ -15,4 +15,21 @@ double SparseMatrix::getVal(size_t row, size_t col) const {
 std::shared_ptr<Matrix> SparseMatrix::clone() const {
     return std::make_shared<SparseMatrix>(*this);
 }
+
+std::string SparseMatrix::print(const std::string &matrixName) const {
+    std::ostringstream result;
+    result << (matrixName != "" ? "[" + matrixName + "] " : "");
+    result << "Sparse Matrix (" << m_rows << ":" << m_cols << ")\n";
+    for (size_t i = 0; i < m_rows; ++i) {
+        result << "| ";
+        for (size_t j = 0; j < m_cols; ++j) {
+            if (m_data.find(std::make_pair(i, j)) != m_data.end())
+                result << m_data.at(std::make_pair(i, j)) << " ";
+            else
+                result << "0 ";
+        }
+        result << "|\n";
+    }
+    return result.str();
+}
 
