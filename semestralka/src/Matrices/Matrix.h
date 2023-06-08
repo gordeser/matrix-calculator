216f56902f92134aaffb304a35016f77d406bbfc
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 11:29:02 +0200

    Matrix header file is modified

--- semestralka/src/Matrices/Matrix.h
+++ semestralka/src/Matrices/Matrix.h
@@ -2,24 +2,33 @@
 // Created by gordeser on 11.05.2023.
 //
 
+#ifndef PA2_MATRIX_H
+#define PA2_MATRIX_H
 
 #include <cstdlib>
+#include <sstream>
+#include <vector>
+#include <math.h>
+#include <vector>
 #include <memory>
 
 class Matrix {
-    double countDeterminant(std::vector <std::vector <double>> matrix, size_t n) const;
+    double countDeterminant(std::vector <std::vector <double>> matrix, size_t size) const;
 protected:
-    std::size_t m_rows;
-    std::size_t m_cols;
+    size_t m_rows;
+    size_t m_cols;
+    size_t countMaxDigits(size_t column) const;
 public:
-    Matrix(std::size_t numRows, std::size_t numCols);
+    Matrix(size_t numRows, size_t numCols);
     virtual ~Matrix() {}
-    std::size_t numRows() const;
-    std::size_t numCols() const;
-    virtual std::size_t rank() const;
+    size_t numRows() const;
+    size_t numCols() const;
+    virtual size_t rank() const;
     virtual double determinant() const;
-    virtual double getVal(std::size_t row, std::size_t col) const = 0;
+    virtual double getVal(size_t row, size_t col) const = 0;
     virtual std::shared_ptr<Matrix> clone() const = 0;
-    virtual void print() const = 0;
-    virtual void printToFile() const = 0;
+    virtual std::string print(const std::string &matrixName) const = 0;
+    virtual std::string printToFile(const std::string &matrixName) const = 0;
 };
+
+#endif //PA2_MATRIX_H
