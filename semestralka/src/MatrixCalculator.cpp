1b0b2dad5c13886f4febe73ae0993b67440351ee
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 12:10:40 +0200

    MatrixCalculator runApplication method is implemented

--- semestralka/src/MatrixCalculator.cpp
+++ semestralka/src/MatrixCalculator.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "MatrixCalculator.h"
+#include "Exceptions/ExporterException.h"
+#include "Exceptions/MatrixException.h"
+#include "Exceptions/OperationException.h"
+#include "Exceptions/ParserException.h"
+#include "Exceptions/StorageException.h"
+
+void MatrixCalculator::runApplication() {
+    showGreeting();
+    while (true) {
+        m_console.showText("\n");
+        m_console.showText("$ ");
+        std::string input = m_console.getInput();
+
+        if (input == "quit" || input == "exit")
+            break;
+
+        try {
+            m_parser.parseInput(input);
+        } catch (ExporterException &e) {
+            m_console.showText(e.what());
+        } catch (MatrixException &e) {
+            m_console.showText(e.what());
+        } catch (OperationException &e) {
+            m_console.showText(e.what());
+        } catch (ParserException &e) {
+            m_console.showText(e.what());
+        } catch (StorageException &e) {
+            m_console.showText(e.what());
+        }
+    }
+    showFarewall();
+}
+
