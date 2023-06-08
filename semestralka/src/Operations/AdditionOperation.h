ddf19234d3c2f4573f1495e3c854c7912a0646f5
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-08 12:55:02 +0200

    Forgot that addition operation can be execute on both matrices

--- semestralka/src/Operations/AdditionOperation.h
+++ semestralka/src/Operations/AdditionOperation.h
@@ -9,8 +9,8 @@
 
 class AdditionOperation : public BinaryOperation {
 public:
-    std::vector <std::vector <double>> execute(const std::shared_ptr<Matrix> &a, const std::shared_ptr<Matrix> &b) override;
-    std::vector <std::vector <double>> execute(const std::shared_ptr<Matrix> &a, double b) override;
+    AdditionOperation(const std::shared_ptr <Matrix> &lhs, const std::shared_ptr <Matrix> &rhs);
+    std::vector <std::vector <double>> execute() override;
 };
 
 #endif //PA2_ADDITIONOPERATION_H
