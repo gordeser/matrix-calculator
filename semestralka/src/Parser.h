6e9db8203d242da84ec7838e3fb3f8ed8de70a3e
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-09 21:40:40 +0200

    Parser documentation is written

--- semestralka/src/Parser.h
+++ semestralka/src/Parser.h
@@ -10,25 +10,77 @@
 #include "Utilities.h"
 #include "Exports/TextExport.h"
 
+/** @class Parse user inputs to execute operations */
 class Parser {
+    /** @var storage to save matrices */
     MatrixStorage m_storage;
+    /** @var to work with console */
     ConsoleUI m_console;
+    /** @var to use utilities */
     Utilities m_utilities;
+    /** @var to export some text */
     TextExport m_textexport;
 
+    /**
+     * Method that execute given operations on matrices
+     * @param operations that need to be executed
+     * @return shared smart-pointer to the matrix after executed operations
+     */
     std::shared_ptr<Matrix> executeOperations(std::vector <std::string> &operations);
+    /** Method that prints help message */
     void printHelp();
+    /**
+     * Method that prints given elements
+     * @param elements vector to print
+     */
     void printElements(const std::vector <std::string> &elements);
+    /** Method that prints all elements in database */
     void printAllElements();
+    /**
+     * Method that exports given elements into file
+     * @param filename in that export elements
+     * @param elements vector to export
+     */
     void exportElements(const std::string &filename, const std::vector <std::string> &elements);
+    /**
+     * Method to export all elements in database into file
+     * @param filename in that export elemnts
+     */
     void exportAllElements(const std::string &filename);
+    /**
+     * Method that deletes given elements from database
+     * @param elements to delete
+     */
     void deleteElements(const std::vector <std::string> &elements);
+    /**
+     * Method that deletes all elements from database
+     */
     void deleteAllElements();
-    void importElements(const std::vector<std::string> &elements);
+    /**
+     * Method that imports matrices from given filenames
+     * @param filenames vector to import
+     */
+    void importElements(const std::vector<std::string> &filenames);
+    /**
+     * Method that scan elements of matrix with given name
+     * @param name of matrix
+     */
     void scanElements(const std::string &name);
+    /**
+     * Method that prints determinants of given matrices
+     * @param elements vector to print their determinants
+     */
     void printDet(const std::vector <std::string> &elements);
+    /**
+     * Method that prints ranks of given matrices
+     * @param elements vector to print their ranks
+     */
     void printRank(const std::vector <std::string> &elements);
 public:
+    /**
+     * Method that parses user's input
+     * @param input to parse
+     */
     void parseInput(std::string input);
 };
 
