d11b297e908ccecab216b1dbdd3a843e9804400d
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 12:39:16 +0200

    JoiningDownOperation documentation is written

--- semestralka/src/Operations/JoiningDownOperation.h
+++ semestralka/src/Operations/JoiningDownOperation.h
@@ -7,9 +7,19 @@
 
 #include "BinaryOperation.h"
 
+/** @class inherits from BinaryOperation to represent joining rows operation */
 class JoiningDownOperation : public BinaryOperation {
 public:
+    /**
+     * Constructor to set matrices
+     * @param lhs matrix on left hand side
+     * @param rhs matrix on right hand side
+     */
     JoiningDownOperation(const std::shared_ptr <Matrix> &lhs, const std::shared_ptr <Matrix> &rhs);
+    /**
+     * Overridden method to execute joining rows operation
+     * @return values of matrix after operation
+     */
     std::vector <std::vector <double>> execute() override;
 };
 
