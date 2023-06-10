c3baf15fcd6abfd82477fa476e406c6d85f1a4f2
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 10:22:07 +0200

    ExporterException documentation is written

--- semestralka/src/Exceptions/ExporterException.h
+++ semestralka/src/Exceptions/ExporterException.h
@@ -9,10 +9,20 @@
 #include <exception>
 #include <string>
 
+/** @class to handle all exceptions in export section */
 class ExporterException : public std::exception {
+    /** @var description of error */
     std::string m_message;
 public:
+    /**
+     * Constructor
+     * @param message describes error
+     */
     ExporterException(const std::string &message);
+    /**
+     * Method that returns text of error
+     * @return c-string that contains text of error
+     */
     virtual const char *what() const noexcept override;
 };
 
