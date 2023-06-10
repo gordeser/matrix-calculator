6622caf7892eab3fb53e91153cfcf48467320104
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 11:33:13 +0200

    SparseMatrix documentation is written

--- semestralka/src/Matrices/SparseMatrix.h
+++ semestralka/src/Matrices/SparseMatrix.h
@@ -8,13 +8,41 @@
 #include "Matrix.h"
 #include <map>
 
+/** @class inherits from class Matrix to represent sparse type of matrix */
 class SparseMatrix : public Matrix {
+    /** @var contains values of matrix in map, where key is pair of row and column of value, value is value of matrix */
     std::map <std::pair<size_t, size_t>, double> m_data;
 public:
+    /**
+     * Constructor to set count of rows, count of columns and values of matrix
+     * @param numRows count of rows
+     * @param numCols count of columns
+     * @param data values of matrix
+     */
     SparseMatrix(size_t numRows, size_t numCols, std::map <std::pair<size_t, size_t>, double> data);
+    /**
+     * Overridden method to get some value of matrix
+     * @param row of value
+     * @param col of value
+     * @return value of matrix
+     */
     double getVal(size_t row, size_t col) const override;
+    /**
+     * Overridden method to make deep copy of matrix
+     * @return shared smart-pointer on matrix copy
+     */
     std::shared_ptr<Matrix> clone() const override;
+    /**
+     * Overridden method to get information about matrix
+     * @param matrixName information to get
+     * @return string that contains information about matrix
+     */
     std::string print(const std::string &matrixName) const override;
+    /**
+     * Overridden method to get information about matrix to export it in file
+     * @param matrixName information to get
+     * @return string that contains information about matrix in handy way
+     */
     std::string printToFile(const std::string &matrixName) const override;
 };
 
