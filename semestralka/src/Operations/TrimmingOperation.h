455add0de1e72db24d63fc2c09434f41037cd671
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 12:40:05 +0200

    TrimmingOperation documentation is written

--- semestralka/src/Operations/TrimmingOperation.h
+++ semestralka/src/Operations/TrimmingOperation.h
@@ -7,11 +7,31 @@
 
 #include "Operation.h"
 
+/** @class inherits from Operation to represent trimming operation */
 class TrimmingOperation : public Operation {
 protected:
-    size_t m_rows, m_cols, m_offsetRows, m_offsetCols;
+    /** @var count of rows of new matrix */
+    size_t m_rows;
+    /** @var count of columns of new matrix*/
+    size_t m_cols;
+    /** @var rows offset */
+    size_t m_offsetRows;
+    /** @var columns offset */
+    size_t m_offsetCols;
 public:
+    /**
+     * Constructor to set matrix, new count of rows and columns, rows and columns offsets
+     * @param lhs matrix on left hand side
+     * @param rows count of rows of new matrix
+     * @param cols count of columns of new matrix
+     * @param offsetRows
+     * @param offsetCols
+     */
     TrimmingOperation(std::shared_ptr<Matrix> &lhs, size_t rows, size_t cols, size_t offsetRows, size_t offsetCols);
+    /**
+     * Overridden method to execute trimming operation
+     * @return values of matrix after operation
+     */
     std::vector <std::vector <double>> execute();
 };
 
