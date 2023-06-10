964635e4c3b7b0a23ee808e5118eaf96581a121d
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 10:33:38 +0200

    TextExport documentation is written

--- semestralka/src/Exports/TextExport.h
+++ semestralka/src/Exports/TextExport.h
@@ -8,10 +8,23 @@
 #include "Export.h"
 #include "../Utilities.h"
 
+/** @class inherits from class Export to represent text type of export */
 class TextExport : public Export {
+    /** @var to contain some useful utilities */
     Utilities m_utilities;
 public:
+    /**
+     * Method that exports matrices to file in text way
+     * @param filename to export matrices
+     * @param names vector of matrices to export
+     * @param storage to export from
+     */
     void exportData(const std::string &filename, const std::vector <std::string> &names, const MatrixStorage &storage) override;
+    /**
+     * Method that imports matrices from file in text way
+     * @param filename to import matrices from
+     * @param storage to import
+     */
     void importData(const std::string &filename, MatrixStorage &storage) override;
 };
 
