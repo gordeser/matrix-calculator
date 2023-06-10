9b6f9d3e234731ad34877910f99b50382d54c960
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 11:33:02 +0200

    Matrix documentation is written

--- semestralka/src/Matrices/Matrix.h
+++ semestralka/src/Matrices/Matrix.h
@@ -12,22 +12,78 @@
 #include <vector>
 #include <memory>
 
+/** @class Abstract class represents types of matrices (dense, sparse, identity, ...) */
 class Matrix {
+    /**
+     * Method to count determinant of matrix
+     * @param matrix in what count determinant
+     * @param size of matrix (number of rows or columns)
+     * @return determinant of matrix
+     */
     double countDeterminant(std::vector <std::vector <double>> matrix, size_t size) const;
 protected:
+    /** @var count of rows */
     size_t m_rows;
+    /** @var count of columns */
     size_t m_cols;
+
+    /** Auxiliary method that counts max length of digits of matrix column
+     * @param column in which count max length of digits
+     * @return max length of digits
+     * */
     size_t countMaxDigits(size_t column) const;
 public:
+    /**
+     * Construtor to set count of rows and count of columns
+     * @param numRows count of rows
+     * @param numCols count of columns
+     */
     Matrix(size_t numRows, size_t numCols);
+    /** Virtual destructor */
     virtual ~Matrix() {}
+    /**
+     * Method to get count of rows
+     * @return count of rows
+     */
     size_t numRows() const;
+    /**
+     * Method to get count of columns
+     * @return count of columns
+     */
     size_t numCols() const;
+    /**
+     * Virtual method to count rank of matrix
+     * @return rank of matrix
+     */
     virtual size_t rank() const;
+    /**
+     * Virtual method to count determinant of matrix
+     * @return determinant of matrix
+     */
     virtual double determinant() const;
+    /**
+     * Virtual method to get some value of matrix
+     * @param row of value
+     * @param col of value
+     * @return value of matrix
+     */
     virtual double getVal(size_t row, size_t col) const = 0;
+    /**
+     * Virtual method to make deep copy of matrix
+     * @return shared smart-pointer on matrix copy
+     */
     virtual std::shared_ptr<Matrix> clone() const = 0;
+    /**
+     * Virtual method to get information about matrix
+     * @param matrixName information to get
+     * @return string that contains information about matrix
+     */
     virtual std::string print(const std::string &matrixName) const = 0;
+    /**
+     * Virtual method to get information about matrix to export it in file
+     * @param matrixName information to get
+     * @return string that contains information about matrix in handy way
+     */
     virtual std::string printToFile(const std::string &matrixName) const = 0;
 };
 
