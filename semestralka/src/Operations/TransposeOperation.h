895fa4b0cb43b3ab133c4d70f943539cf4d83bf7
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 12:39:57 +0200

    TransposeOperation documentation is written

--- semestralka/src/Operations/TransposeOperation.h
+++ semestralka/src/Operations/TransposeOperation.h
@@ -7,9 +7,18 @@
 
 #include "Operation.h"
 
+/** @class inherits from Operation to represent transpose operation */
 class TransposeOperation : public Operation {
 public:
+    /**
+     * Constructor to set matrix
+     * @param lhs matrix on left hand side
+     */
     TransposeOperation(std::shared_ptr <Matrix> lhs);
+    /**
+     * Overridden method to execute transpose operation
+     * @return values of matrix after operation
+     */
     std::vector <std::vector <double>> execute() override;
 };
 
