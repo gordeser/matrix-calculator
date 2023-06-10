ff5c4a84c6d77888a65ac1c6e83ef51dc701757a
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 11:32:29 +0200

    DenseMatrix documentation is written

--- semestralka/src/Matrices/DenseMatrix.h
+++ semestralka/src/Matrices/DenseMatrix.h
@@ -7,13 +7,41 @@
 
 #include "Matrix.h"
 
+/** @class inherits from class Matrix to represent dense type of matrix */
 class DenseMatrix : public Matrix {
+    /** @var contains values of matrix */
     std::vector<std::vector <double>> m_data;
 public:
+    /**
+     * Constructor to set count of rows, count of columns and values of matrix
+     * @param numRows count of rows
+     * @param numCols count of columns
+     * @param data values of matrix
+     */
     DenseMatrix(size_t numRows, size_t numCols, std::vector<std::vector <double>> data);
+    /**
+     * Overridden method to get some value of matrix
+     * @param row of value
+     * @param col of value
+     * @return value of matrix
+     */
     double getVal(size_t row, size_t col) const override;
+    /**
+     * Overridden method to make deep copy of matrix
+     * @return shared smart-pointer on matrix copy
+     */
     std::shared_ptr<Matrix> clone() const override;
+    /**
+     * Overridden method to get information about matrix
+     * @param matrixName information to get
+     * @return string that contains information about matrix
+     */
     std::string print(const std::string &matrixName) const override;
+    /**
+     * Overridden method to get information about matrix to export it in file
+     * @param matrixName information to get
+     * @return string that contains information about matrix in handy way
+     */
     std::string printToFile(const std::string &matrixName) const override;
 };
 
