63d0ef70a8aa0e0792ff8d34dc2880a29cc7e9d5
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 12:39:24 +0200

    JoiningRightOperation documentation is written

--- semestralka/src/Operations/JoiningRightOperation.h
+++ semestralka/src/Operations/JoiningRightOperation.h
@@ -7,9 +7,19 @@
 
 #include "BinaryOperation.h"
 
+/** @class inherits from BinaryOperation to represent joining columns operation */
 class JoiningRightOperation : public BinaryOperation {
 public:
+    /**
+     * Constructor to set matrices
+     * @param lhs matrix on left hand side
+     * @param rhs matrix on right hand side
+     */
     JoiningRightOperation(const std::shared_ptr <Matrix> &lhs, const std::shared_ptr <Matrix> &rhs);
+    /**
+     * Overridden method to execute joining columns operation
+     * @return values of matrix after operation
+     */
     std::vector <std::vector <double>> execute() override;
 };
 
