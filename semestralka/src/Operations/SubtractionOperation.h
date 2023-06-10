b14f2ce1b40d43d0211f76b4be4f62c6a12062aa
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 12:39:51 +0200

    SubstractionOperation documentation is written

--- semestralka/src/Operations/SubtractionOperation.h
+++ semestralka/src/Operations/SubtractionOperation.h
@@ -7,9 +7,19 @@
 
 #include "BinaryOperation.h"
 
+/** @class inherits from BinaryOperation to represent substraction operation */
 class SubtractionOperation  : public BinaryOperation {
 public:
+    /**
+     * Constructor to set matrices
+     * @param lhs matrix on left hand side
+     * @param rhs matrix on right hand side
+     */
     SubtractionOperation(const std::shared_ptr<Matrix> &lhs, const std::shared_ptr<Matrix> &rhs);
+    /**
+     * Overridden method to execute substraction operation
+     * @return values of matrix after operation
+     */
     std::vector <std::vector <double>> execute() override;
 };
 
