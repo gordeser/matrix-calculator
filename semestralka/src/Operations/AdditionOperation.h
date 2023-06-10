a1948e012789f65170c905d2093f5d8e3ecd17bd
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 12:38:28 +0200

    AdditionOperation documentation is written

--- semestralka/src/Operations/AdditionOperation.h
+++ semestralka/src/Operations/AdditionOperation.h
@@ -7,9 +7,19 @@
 
 #include "BinaryOperation.h"
 
+/** @class inherits from class BinaryOperation to represent addition operation */
 class AdditionOperation : public BinaryOperation {
 public:
+    /**
+     * Constructor to set left and right hand side matrices
+     * @param lhs left hand side matrix
+     * @param rhs right hand side matrix
+     */
     AdditionOperation(const std::shared_ptr <Matrix> &lhs, const std::shared_ptr <Matrix> &rhs);
+    /**
+     * Overridden matrix that executes addition operation
+     * @return matrix values after addition operaation
+     */
     std::vector <std::vector <double>> execute() override;
 };
 
