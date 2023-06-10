52429b56da3bb86f97ed786898209d5572371d0f
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 12:38:38 +0200

    BinaryOperation documentation is written

--- semestralka/src/Operations/BinaryOperation.h
+++ semestralka/src/Operations/BinaryOperation.h
@@ -7,11 +7,22 @@
 
 #include "Operation.h"
 
+/** @class Abstract class that inherits from class Operation to represent binary operation */
 class BinaryOperation : public Operation {
 protected:
+    /** @var matrix on right hand side */
     std::shared_ptr <Matrix> m_rhs;
 public:
+    /**
+     * Constructor to set matrices on left and right hand sides
+     * @param lhs left hand side matrix
+     * @param rhs right hand side matrix
+     */
     BinaryOperation(std::shared_ptr <Matrix> lhs, std::shared_ptr <Matrix> rhs);
+    /**
+     * Virtual method that executes operation
+     * @return values of matrix after operation
+     */
     virtual std::vector <std::vector <double>> execute() = 0;
 };
 
