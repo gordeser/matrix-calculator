3fe6492a1f80bb009c802c9cf26a38d8adb57516
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 12:39:33 +0200

    MultiplicationNumberOperation documentation is written

--- semestralka/src/Operations/MultiplicationNumberOperation.h
+++ semestralka/src/Operations/MultiplicationNumberOperation.h
@@ -7,11 +7,22 @@
 
 #include "Operation.h"
 
+/** @class inherits from Operation to represent multiplication by a number operation */
 class MultiplicationNumberOperation : public Operation {
 protected:
+    /** @var scalar on right hand side */
     double m_rhs;
 public:
+    /**
+     * Constructor to set matrix and scalar
+     * @param lhs matrix on left hand side
+     * @param rhs scalar on right hand side
+     */
     MultiplicationNumberOperation(const std::shared_ptr <Matrix> &lhs, double rhs);
+    /**
+     * Overridden method to execute multiplication by a number operation
+     * @return values of matrix after operation
+     */
     std::vector <std::vector <double>> execute() override;
 };
 
