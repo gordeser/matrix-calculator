06a5be7e79b3a061e83da2aa2461be156371b7eb
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 12:39:44 +0200

    Operation documentation is written

--- semestralka/src/Operations/Operation.h
+++ semestralka/src/Operations/Operation.h
@@ -7,12 +7,23 @@
 
 #include "../Matrices/Matrix.h"
 
+/** @class Abstract class that represents unary operation */
 class Operation {
 protected:
+    /** @var matrix on left hand side */
     std::shared_ptr<Matrix> m_lhs;
 public:
+    /**
+     * Constructor to set matrix on left hand side
+     * @param lhs left hand side matrix
+     */
     Operation(std::shared_ptr<Matrix> lhs);
+    /** Virtual destructor */
     virtual ~Operation() {}
+    /**
+     * Virtual method that executes operation
+     * @return values of matrix after operation
+     */
     virtual std::vector <std::vector <double>> execute() = 0;
 };
 
