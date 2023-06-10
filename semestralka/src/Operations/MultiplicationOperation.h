fd0d50d6601e10d85283b4525e99426e0d197b59
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-10 12:39:40 +0200

    MultiplicationOperation documentation is written

--- semestralka/src/Operations/MultiplicationOperation.h
+++ semestralka/src/Operations/MultiplicationOperation.h
@@ -7,9 +7,19 @@
 
 #include "BinaryOperation.h"
 
+/** @class inherits from BinaryOperation to represent multiplication operation */
 class MultiplicationOperation : public BinaryOperation {
 public:
+    /**
+     * Constructor to set matrices
+     * @param lhs matrix on left hand side
+     * @param rhs matrix on right hand side
+     */
     MultiplicationOperation(const std::shared_ptr <Matrix> &lhs, const std::shared_ptr <Matrix> &rhs);
+    /**
+     * Overridden method to execute multiplication operation
+     * @return values of matrix after operation
+     */
     std::vector <std::vector <double>> execute() override;
 };
 
