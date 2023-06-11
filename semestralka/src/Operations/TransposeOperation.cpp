27fbf846166fbd9e3a66a51663a657bf2962b5fb
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-06-11 13:34:41 +0200

    fix transpose operation

--- semestralka/src/Operations/TransposeOperation.cpp
+++ semestralka/src/Operations/TransposeOperation.cpp
@@ -7,9 +7,9 @@
 TransposeOperation::TransposeOperation(std::shared_ptr<Matrix> lhs) : Operation(lhs) {}
 
 std::vector <std::vector <double>> TransposeOperation::execute() const {
-    std::vector <std::vector <double>> result(m_lhs->numRows(), std::vector<double> (m_lhs->numCols(), 0));
-    for (size_t i = 0; i < m_lhs->numRows(); ++i)
-        for (size_t j = 0; j < m_lhs->numCols(); ++j)
+    std::vector <std::vector <double>> result(m_lhs->numCols(), std::vector<double> (m_lhs->numRows(), 0));
+    for (size_t i = 0; i < m_lhs->numCols(); ++i)
+        for (size_t j = 0; j < m_lhs->numRows(); ++j)
             result[i][j] = m_lhs->getVal(j, i);
 
     return result;
